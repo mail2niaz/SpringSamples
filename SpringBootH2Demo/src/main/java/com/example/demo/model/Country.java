1c0d69290852fa61db580f7acd3e28c1bae43b3d
mail2niaz <mail2niazahamed@gmail.com>
2022-07-07 17:47:41 +0530

    auto generated ids

--- SpringBootH2Demo/src/main/java/com/example/demo/model/Country.java
+++ SpringBootH2Demo/src/main/java/com/example/demo/model/Country.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Country {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
     private String firstname;
--- SpringBootH2Demo/src/main/java/com/example/demo/repository/UserRestRepo.java
+++ SpringBootH2Demo/src/main/java/com/example/demo/repository/UserRestRepo.java
@@ -2,9 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "country", path = "country")
-public interface UserRestRepo extends JpaRepository<Country, Integer> {
+public interface UserRestRepo extends CrudRepository<Country, Integer> {
 
 }
